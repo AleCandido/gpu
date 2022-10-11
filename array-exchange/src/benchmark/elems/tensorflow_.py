91d81a73055ccab0669fb4b0a4b03dd626057cc0
Alessandro Candido <candido.ale@gmail.com>
2022-10-11 16:31:47 +0200

    Implement pytorch and tensorflow basic interface

--- array-exchange/src/benchmark/elems/tensorflow_.py
+++ array-exchange/src/benchmark/elems/tensorflow_.py
@@ -1,1 +1,15 @@
 import tensorflow as tf
+
+from .utils import prod
+
+
+def arange(shape):
+    return tf.reshape(tf.range(prod(shape)), shape)
+
+
+def random(shape):
+    return tf.random.uniform(shape=shape)
+
+
+def saxpy(x, y, a):
+    return a * x + y
--- array-exchange/src/benchmark/elems/torch_.py
+++ array-exchange/src/benchmark/elems/torch_.py
@@ -1,1 +1,18 @@
 import torch
+import torch.random
+
+from .utils import prod
+
+cuda = torch.device("cuda")
+
+
+def arange(shape):
+    return torch.arange(prod(shape), device=cuda).reshape(shape)
+
+
+def random(shape):
+    return torch.rand(prod(shape), device=cuda).reshape(shape)
+
+
+def saxpy(x, y, a):
+    return a * x + y
